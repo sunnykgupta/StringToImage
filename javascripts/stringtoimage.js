f6ebe073795d8051a55bc99fb1718a457996ba3f
Sunny R Gupta <sunnykgupta@users.noreply.github.com>
2016-09-02 18:09:46 +0530

    Update stringtoimage.js

--- javascripts/stringtoimage.js
+++ javascripts/stringtoimage.js
@@ -8,7 +8,7 @@ $(function() {
     ];
 
     var seedFromString = function(seed, test_name, options) {
-        word = md5("" + seed + test_name)[0];
+        word = CryptoJS.MD5("" + seed + test_name).word[0];
         i = Math.abs(word);
         return i;
     }
